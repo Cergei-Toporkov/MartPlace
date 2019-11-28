44533286f9094a7fa4c83ceee9175feb37075369
Cergei-Toporkov <Cergei-Toporkov@rambler.ru>
2019-11-28 11:26:05 +0500

    added section choice

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require("gulp"),
 
 gulp.task("sass", function() {
   return gulp
-    .src("app/scss/style.scss")
+    .src("app/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(
@@ -25,7 +25,7 @@ gulp.task("script", function() {
   return gulp
     .src([
       "node_modules/slick-carousel/slick/slick.js",
-      "node_modules/magnific-popp/dist/jquery.magnific-popup.js",
+      "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
       "node_modules/mixitup/dist/mixitup.js"
     ])
     .pipe(concat("libs.min.js"))
@@ -62,7 +62,7 @@ gulp.task("browser-sync", function() {
 });
 
 gulp.task("watch", function() {
-  gulp.watch("app/scss/style.scss", gulp.parallel("sass"));
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("app/*.html", gulp.parallel("html"));
   gulp.watch("app/js/*.js", gulp.parallel("js"));
 });
