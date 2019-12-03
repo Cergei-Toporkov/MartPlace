26d70c3a271e7b3aacd7e9b66a782c054689a591
Cergei-Toporkov <Cergei-Toporkov@rambler.ru>
2019-12-03 23:50:39 +0500

    added all not footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,16 @@
 $(function() {
+  var mixer = mixitup(".products__inner-box");
   $(".slider__item-info-rateyo").rateYo({
     rating: 4.5,
-    starWidth: "16px",
-    readOnly: true
+    starWidth: "16px"
   });
+  $(".product__item-info-rateyo").rateYo({
+    rating: 4.5,
+    starWidth: "14px"
+  });
+
   $(".slider__inner").slick({
+    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false
@@ -14,5 +20,18 @@ $(function() {
   });
   $(".slider__top-next").on("click", function() {
     $(".slider__inner").slick("slickNext");
+  });
+
+  $(".slider__followers-inner").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false
+  });
+  $(".slider__followers-top-prev").on("click", function() {
+    $(".slider__followers-inner").slick("slickPrev");
+  });
+  $(".slider__followers-top-next").on("click", function() {
+    $(".slider__followers-inner").slick("slickNext");
   });
 });
